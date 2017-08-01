23f0adc374677894ce9aaf779e891482c1b8fdb1
David Oram <david.oram99@gmail.com>
2017-08-02 08:18:40 +1200

    Added support for postgres db existence, tcp server listening. Added a homepage

--- README.md
+++ README.md
@@ -50,17 +50,43 @@ Type `./web-rendezvous --help` to see usage:
 Usage of ./web-rendezvous:
   -port string
         Port to listen on (default "8080")
+  -postgresURL string
+        Postgres URL of the form 'postgres://<username>:<password>@<host>:<port>/{{.}}[?<connection-string-params]'. {{.}} will be replaced with the db name (default "postgres://postgres:pgpass@postgres:5432/{{.}}?sslmode=disable")
   -timeout float
-        Timeout in seconds (default 30)
-```
+        Timeout in seconds (default 30)```
+
+## Web interface
+
+Open the homepage of web-rendezvous to see a list of keys that are currently waiting, or experienced a timeout.
+
+## Synchronize on a variable
 
 If you run:
 
   `curl -X GET --max-time 600   http://localhost:8080/abc`
 
-.. then this call will block, until you open another session and run:
+.. then web-rendezvous will enter a loop, repeatedly attempting to see if another caller has `POST/PUT` variable `abc`.  If successful it will return 200, otherwise when the timeout period expires it will return 404.
+
+To have another caller set variable `abc` type the following (in another terminal session):
 
   `curl -X PUT http://localhost:8080/abc`
 
-The second call will cause the first to return with 200.
+The `PUT` for variable `abc` will cause the first caller to unblock, and return with 200, and the two processes can be synchronized to a specific point in time.
 
+## Synchronize on Postgres database creation
+
+If you run:
+
+  `curl -X GET --max-time 600   http://localhost:8080/_postgres/mydb`
+
+.. then web-rendezvous will enter a loop, repeatedly attempting to connect to the database server using the connection string specified in the `--postgresURL` command line parameter, and run the query `SELECT 1` against `mydb`. If successful it will return 200, otherwise when the timeout period expires it will return 404.
+
+## Port listening existence
+
+If you run:
+
+  `curl -X GET --max-time 600   http://localhost:8080/_port/host/port`
+
+.. then web-rendezvous will enter a loop, repeatedly attempting toopne a TCP connection to `host`:`port`. If successful it will return 200 immediately, if not block until the timeout period expires & return 404.
+
+End.
--- main.go
+++ main.go
@@ -1,25 +1,93 @@
 package main
 
 import (
+	"bytes"
+	"database/sql"
 	"flag"
 	"fmt"
+	"html/template"
+	"net"
 	"net/http"
 	"strings"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 var timeout float64
+var postgresURL string
 var keys = make(map[string]bool)
+var waiting = make(map[string]bool)
+
+// Check if a key exists.
+// key:  _postgres/dbname - Will check if database 'dbname' exists
+// key:  _port/host/port  - Will check if can open a TCP connection to that host/port
+// All other keys will simply check if keys[key] exists
+func keyExists(key string) bool {
+	if strings.HasPrefix(key, "/_postgres/") {
+		// Postgres connection?
+		split := strings.Split(key, "/")
+		if len(split) != 3 {
+			fmt.Printf("'%s' : Error: unable to parse postgres db from : '%s' ...\n", key, key)
+			return false
+		}
+		database := split[2]
+		fmt.Printf("'%s' : Testing for postgres db: '%s' ...\n", key, database)
+		t := template.Must(template.New("dbConnection").Parse(postgresURL))
+		var tpl bytes.Buffer
+		if err := t.Execute(&tpl, database); err != nil {
+			fmt.Printf("'%s' : Error generating postgres connection template: '%s', error: '%s' ...\n", key, postgresURL, err)
+			return false
+		}
+
+		db, err := sql.Open("postgres", tpl.String())
+		defer db.Close()
+		if err == nil {
+			if _, err := db.Query("SELECT 1"); err != nil {
+				// fmt.Printf("'%s' : Can't run query 'select 1' in postgres db: '%s', error: '%s' ...\n", key, database, err)
+				return false
+			}
+		} else {
+			fmt.Printf("'%s' : Can't connect to postgres db: '%s', error: '%s' ...\n", key, database, err)
+			return false
+		}
+		fmt.Printf("'%s' : postgres db '%s' created - OK\n", key, database)
+		return true
+
+	} else if strings.HasPrefix(key, "/_port/") {
+		// Port connection?
+		split := strings.Split(key, "/")
+		if len(split) != 4 {
+			fmt.Printf("'%s' : Error: unable to parse host/port from : '%s' ...\n", key, key)
+			return false
+		}
+		host := split[2]
+		port := split[3]
+		fmt.Printf("'%s' : Connecting to host:port: '%s:%s' ...\n", key, host, port)
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
+		if err != nil {
+			// fmt.Printf("'%s' : Can't connect to host/port: '%s:%s', error: '%s\n", key, host, port, err)
+			return false
+		}
+		conn.Close()
+		fmt.Printf("'%s' : host:port: '%s:%s' listening - OK\n", key, host, port)
+		return true
+	} else {
+		return keys[key]
+	}
+}
 
 func waitFor(key string) bool {
+	waiting[key] = true
 	fmt.Printf("'%s' : Waiting ...\n", key)
 	t := time.Now()
 	for {
-		if keys[key] {
+		if keyExists(key) {
 			fmt.Printf("'%s' : Waiting - OK\n", key)
+			delete(waiting, key)
 			return true
 		}
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 		if time.Since(t).Seconds() > timeout {
 			fmt.Printf("'%s' : Waiting - Timeout\n", key)
 			return false
@@ -36,12 +104,23 @@ func mark(key string) bool {
 func handler(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path
 	if strings.ToLower(r.Method) == "get" {
-		if !waitFor(key) {
+		if key == "/" {
+			fmt.Fprintln(w, "web-rendezvous")
+			fmt.Fprintln(w, "\nCurrently waiting for + recently failed:")
+			for k := range waiting {
+				fmt.Fprintln(w, "\t", k)
+			}
+			fmt.Fprintln(w, "Total :", len(waiting))
+		} else if key == "/favicon.ico" {
 			w.WriteHeader(404)
-			fmt.Fprintf(w, "Timeout : '%s'", key)
 		} else {
-			w.WriteHeader(200)
-			fmt.Fprint(w, "OK")
+			if !waitFor(key) {
+				w.WriteHeader(404)
+				fmt.Fprintf(w, "Timeout : '%s'", key)
+			} else {
+				w.WriteHeader(200)
+				fmt.Fprint(w, "OK")
+			}
 		}
 	} else if strings.ToLower(r.Method) == "put" || strings.ToLower(r.Method) == "post" {
 		mark(key)
@@ -56,6 +135,7 @@ func main() {
 	var port string
 	flag.StringVar(&port, "port", "8080", "Port to listen on")
 	flag.Float64Var(&timeout, "timeout", 30.0, "Timeout in seconds")
+	flag.StringVar(&postgresURL, "postgresURL", "postgres://postgres:pgpass@postgres:5432/{{.}}?sslmode=disable", "Postgres URL of the form 'postgres://<username>:<password>@<host>:<port>/{{.}}[?<connection-string-params]'. {{.}} will be replaced with the db name")
 	flag.Parse()
 
 	fmt.Println("web-rendezvous")
